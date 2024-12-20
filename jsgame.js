ec5fadff19aefdd88faeb8217f6fb1b39f858b9c
Luis Montes <monteslu@gmail.com>
2024-12-20 12:55:07 -0700

    initial

---
+++ jsgame.js
@@ -0,0 +1,46 @@
+module.exports = function(RED) {
+  RED.settings.functionGlobalContext.canvas = globalThis.canvas;
+  function JSGameInputNode(config) {
+    RED.nodes.createNode(this, config);
+    const node = this;
+
+    const handler = (data) => {
+      if (data) {
+        data.topic = 'JSGameInput';
+        node.send(data);
+      }
+    };
+
+    if (global.jsGameEmitter) {
+      global.jsGameEmitter.on('update', handler);
+    }
+
+    node.on("close", () => {
+      if (global.jsGameEmitter) {
+        global.jsGameEmitter.removeListener('update', handler);
+      }
+    });
+  }
+
+  RED.nodes.registerType("jsgame-in", JSGameInputNode);
+
+  function JSGameOut(config) {
+    RED.nodes.createNode(this, config);
+    const node = this;
+    if (globalThis.canvas) {
+      node.status({ fill: "green", shape: "dot", text: `${globalThis.canvas.width} x ${globalThis.canvas.height}` });
+    }
+    node.on("input", (msg) => {
+      node.log(`Received: ${msg.payload}`);
+      if (globalThis.canvas) {
+        node.status({ fill: "green", shape: "dot", text: `${globalThis.canvas.width} x ${globalThis.canvas.height}` });
+      }
+      if (global.jsGameEmitter) {
+        global.jsGameEmitter.emit('msg', msg);
+      }
+    });
+  }
+
+  RED.nodes.registerType("jsgame-out", JSGameOut);
+};
+
